8738eaa6f8b077fc4fb384bdc2359e19556367f7
Sushmita <36688322+spattanaik9@users.noreply.github.com>
2026-05-23 20:41:03 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #create a hashmap. and iterate for the 2ns string
+        #T:O(n)
+        #S:O(n)
+        if s is None or t is None or len(s)!=len(t):
+            return False
+
+        hashmap = {}
+        for c in s:
+            if c not in hashmap:
+                hashmap[c] = 1
+            else:
+                hashmap[c] += 1
+
+        for c in t:
+            if c not in hashmap or hashmap[c] <=0:
+                return False
+            hashmap[c] -= 1
+
+        for k, v in hashmap.items():
+            if v != 0:
+                return False
+
+        return True
